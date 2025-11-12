9bbaa05b77e4a6899e9c04d3dae2b739e3649d5c
홍범선 <qjatjs123123@naver.com>
2025-11-12 14:56:45 +0900

    feat: Text 공통 컴포넌트 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Text } from "@/src/shared/ui/Text";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <Text weight="bold" size="1" type="title">123</Text>
+    </>
   );
 }
