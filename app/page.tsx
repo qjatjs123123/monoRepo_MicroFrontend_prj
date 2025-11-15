12b870a8ff63c7ab2978eed71f09a13989a1a74b
홍범선 <qjatjs123123@naver.com>
2025-11-16 02:44:29 +0900

    feat: 타이틀 및 파비콘 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 import { Page } from "@/page/Manager";
+import { Metadata } from "next";
 
 interface SearchParamsProps {
   searchParams: Promise<{
     page?: string;
   }>;
 }
+export const metadata: Metadata = {
+  title: "관심기업 관리 서비스",
+  description: "관심 기업을 등록하고 삭제하며 관리하세요.",
+  openGraph: {
+    title: "관심기업 관리 서비스",
+    description: "관심 기업을 등록하고 삭제하며 관리하세요.",
+  },
+  twitter: {
+    title: "관심기업 관리 서비스",
+    description: "관심 기업을 등록하고 삭제하며 관리하세요.",
+  },
+};
 
 export default async function Home({ searchParams }: SearchParamsProps) {
   const { page = "1" } = (await searchParams) ?? {};
-
   return <Page page={page} />;
 }
