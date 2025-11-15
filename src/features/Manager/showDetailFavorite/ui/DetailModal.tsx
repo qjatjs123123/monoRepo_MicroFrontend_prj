e2bcaf489049b5044233fdb1131827521edd0a81
홍범선 <qjatjs123123@naver.com>
2025-11-15 14:13:38 +0900

    상세페이지 모달 디자인 구현

--- src/features/Manager/showDetailFavorite/ui/DetailModal.tsx
+++ src/features/Manager/showDetailFavorite/ui/DetailModal.tsx
@@ -1,9 +1,29 @@
+import { Button } from "@/shared/ui/Button/Button";
+import { EditIcon } from "@/shared/ui/Icon/EditIcon";
 import { Modal } from "@/shared/ui/Modal/Modal";
+import { Text } from "@/shared/ui/Text/Text";
 
 export function DetailModal() {
   return (
-    <Modal>
-      <div>1231</div>
+    <Modal align="right">
+      <div className="w-full md:w-1/2 h-screen bg-white flex flex-col">
+        <div className="flex justify-center py-3">
+          <Text weight="bold">삼성전자</Text>
+        </div>
+
+        <div className="bt border-t border-[var(--color-line-400)] p-2 flex-1">
+          <div className="p-2 border border-[var(--color-line-400)] h-full">
+            213123123123
+          </div>
+        </div>
+
+        <div className="flex justify-end p-2">
+          <Button style="fill" className="flex gap-2">
+            <EditIcon />
+            <Text>수정하기</Text>
+          </Button>
+        </div>
+      </div>
     </Modal>
   );
 }
