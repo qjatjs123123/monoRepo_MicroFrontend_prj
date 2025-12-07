578d146b881b9f259fd13604f714ca8722b4caf9
홍범선 <qjatjs123123@naver.com>
2025-12-08 02:19:39 +0900

    fix: router 안에 넣기

--- apps/shell/src/app/App.tsx
+++ apps/shell/src/app/App.tsx
@@ -1,7 +1,7 @@
 import { TanStackProvider } from "@monorepo/core";
 import { Header } from "@/widgets/header";
 import { Finance } from "@/widgets/finance";
-import { Viewer } from "@/widgets/viewer";// Viewer 페이지 컴포넌트 예시
+import { Viewer } from "@/widgets/viewer"; // Viewer 페이지 컴포넌트 예시
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Banner } from "@/shared/ui";
 
@@ -10,9 +10,8 @@ export default function App() {
     <BrowserRouter>
       <TanStackProvider>
         <Header />
-        <Banner/>
-        
         <Routes>
+          <Banner />
           <Route path="/" element={<Finance />} />
           <Route path="/viewer" element={<Viewer />} />
           <Route path="*" element={<Finance />} /> {/* 기본 페이지 */}
